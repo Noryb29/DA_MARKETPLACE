e71138a40508e69c40c720e7cfd458488b9584be
Noryb29 <esabarabar@gmail.com>
2026-04-29 12:49:53 +0800

    REWRITES CONFIG REWRITE

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ const app = express()
 const PORT = process.env.PORT
 const FRONTEND_LINK = process.env.CORS_ORIGIN
 app.use(cors({
-  origin: ['http://localhost:5173', `https://${FRONTEND_LINK}`],
+  // origin: ['http://localhost:5173', `https://${FRONTEND_LINK}`],
+  origin:'*',
   credentials: true
 }))
 app.use(express.json())
